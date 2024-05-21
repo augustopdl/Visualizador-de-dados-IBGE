daa2367280d630b36e116e58971d085611df4c8d
Augusto de Lima <64655434+augustopdl@users.noreply.github.com>
2024-05-21 10:50:02 -0300

    Adicionando arquivos .py

---
+++ temp.py
@@ -0,0 +1,19 @@
+from os import listdir, makedirs, rename
+from os.path import isdir
+from pathlib import Path
+
+downloads_path = str(Path.home() / "Downloads")
+
+for file in listdir(downloads_path)[:-1]:
+    aux1 = file.split(' - ')
+    aux2 = aux1[1]
+    aux3 = aux1[-1]
+    aux3 = aux3.removesuffix('.xlsx')
+
+    if not isdir(f'data/{aux3}'):
+        makedirs(f'data/{aux3}')
+
+    rename(
+        fr'{downloads_path}\{file}',
+        fr'data/{aux3}/{aux2}.xlsx'
+    )
